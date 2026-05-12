a7bfeb70c18d655b3421dc91ca1924dd9de49eff
noticemeimhere <noticemeimherebuisness@gmail.com>
2026-05-12 15:58:37 -0700

    Added commands + forced login

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 from twitchAPI.twitch import Twitch
 from twitchAPI.oauth import UserAuthenticator
@@ -10,6 +11,79 @@ load_dotenv()
 
 SECRET_ID = os.getenv('SECRET_ID')
 SECRET_PASS = os.getenv('SECRET_PASS')
+USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
+CHANNEL = 'noticemeimhere'
+TOKEN_FILE = "token.txt"
 
-print(SECRET_ID)
-print(SECRET_PASS)
+whatdoingvar = "@noticemeimhere, SETUP PLEASE!!!!!"
+
+async def on_ready(ready_event: EventData):
+    print('Bot is ready for work, joining channels')
+    await ready_event.chat.join_room(CHANNEL)
+
+async def on_message(msg: ChatMessage):
+    print(f'in {msg.room.name}, {msg.user.name} said: {msg.text}')
+
+# Commands
+
+async def test(cmd: ChatCommand):
+    await cmd.reply(f'pong, {cmd.user.name}')
+
+async def helpcmd(cmd: ChatCommand):
+    await cmd.reply(f"Commands: !ping, !discord, !time, !whatdoing")
+
+async def time(cmd: ChatCommand):
+    await cmd.reply(f"The local time is: {datetime.now().strftime("%H:%M:%S")}")
+
+async def whatdoing(cmd: ChatCommand):
+    await cmd.reply(whatdoingvar)
+
+async def setdoing(cmd: ChatCommand):
+    global whatdoingvar
+
+    if(cmd.user.name != "noticemeimhere"):
+        await cmd.reply("You do not have permissions to use this.")
+    else:
+        whatdoingvar = cmd.parameter
+        await cmd.reply(f"Set whatdoing to {cmd.parameter}!")
+
+
+
+
+
+
+#main
+async def run():
+    twitch = await Twitch(SECRET_ID, SECRET_PASS)
+
+    if os.path.exists(TOKEN_FILE):
+        print("Token found!")
+        with open(TOKEN_FILE, "r") as f:
+            refresh_token = f.read()
+        await twitch.set_user_authentication(None,USER_SCOPE,refresh_token)
+
+    else:
+        print("No token found")
+        auth = UserAuthenticator(twitch,USER_SCOPE)
+        token, refresh_token = await auth.authenticate()
+        with open(TOKEN_FILE, "w") as f:
+            f.write(refresh_token)
+
+    chat = await Chat(twitch)
+    chat.register_event(ChatEvent.READY, on_ready)
+    chat.register_event(ChatEvent.MESSAGE, on_message)
+
+    chat.register_command('ping', test)
+    chat.register_command('help', helpcmd)
+    chat.register_command('time', time)
+    chat.register_command('whatdoing', whatdoing)
+    chat.register_command('setdoing', setdoing)
+    chat.start()
+    try:
+        input('press ENTER to stop\n')
+    finally:
+        chat.stop()
+        await twitch.close()
+
+
+asyncio.run(run())
